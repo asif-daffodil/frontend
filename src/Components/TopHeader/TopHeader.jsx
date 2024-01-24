f80c1450e59acd24d9beeba2159da02f78262838
Asif Abir <88622477+asif-daffodil@users.noreply.github.com>
2024-01-25 04:41:08 +0600

    top header

---
+++ src/Components/TopHeader/TopHeader.jsx
@@ -0,0 +1,26 @@
+import { faFacebook, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+const TopHeader = () => {
+    return (
+        <div className="container">
+            <div className="row py-2">
+                <div className="col-md-6">
+                    <a href="" className="me-2">
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </a>
+                    <a href="" className="text-danger">
+                        <FontAwesomeIcon icon={faYoutube} />
+                    </a>
+                </div>
+                <div className="col-md-6 text-end">
+                    <button className="btn btn-outline-primary me-2 btn-sm">Truck your application</button>
+                    <Link to='/login' className="btn btn-link btn-sm text-decoration-none ">Login</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TopHeader;
--- src/Layout/MainLayouts.jsx
+++ src/Layout/MainLayouts.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Components/Footer/Footer";
 import MainNavbar from "../Components/MainNavbar/MainNavbar";
+import TopHeader from "../Components/TopHeader/TopHeader";
 
 const MainLayouts = () => {
     return (
         <>
+            <TopHeader />
             <MainNavbar />
             <Outlet />
             <Footer />
